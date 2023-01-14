342cb7c2e6655391463cf8950ee4011aae94aacf
YwintA <ywint056@gmail.com>
2023-01-14 10:17:03 +0630

    app.js change

--- app.js
+++ app.js
@@ -9,3 +9,6 @@ app.listen(port,()=>{
 app.get('/',(req,res)=>{
     res.json({message: "wellcome to my node js api"})
 })
+app.get('/food',(req,res)=>{
+    res.json({foods : "pizza,hamburger"})
+})
